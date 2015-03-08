ef65e9df435d919b258305685a8b1f3295d81945
hujun.zhao <hujun.zhao@chukong-inc.com>
2015-03-08 20:09:32 +0800

    new sound effect

---
+++ changeableball/Classes/BuyLifeLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  BuyLifeLayer.cpp
+//  changeableball
+//
+//  Created by huge on 7/3/15.
+//
+//
+
+#include "BuyLifeLayer.h"
---
+++ changeableball/Classes/BuyLifeLayer.h
@@ -0,0 +1,14 @@
+//
+//  BuyLifeLayer.h
+//  changeableball
+//
+//  Created by huge on 7/3/15.
+//
+//
+
+#ifndef __changeableball__BuyLifeLayer__
+#define __changeableball__BuyLifeLayer__
+
+#include <stdio.h>
+
+#endif /* defined(__changeableball__BuyLifeLayer__) */
