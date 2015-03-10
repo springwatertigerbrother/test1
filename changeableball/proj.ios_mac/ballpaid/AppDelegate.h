cc6e99c2e85fe25be27a8808895d590496e00dbb
hujun.zhao <hujun.zhao@chukong-inc.com>
2015-03-10 08:55:32 +0800

    copy charge version bioscopy

---
+++ changeableball/proj.ios_mac/ballpaid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ballpaid
+//
+//  Created by huge on 10/3/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ changeableball/proj.ios_mac/ballpaid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ballpaid
+//
+//  Created by huge on 10/3/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
