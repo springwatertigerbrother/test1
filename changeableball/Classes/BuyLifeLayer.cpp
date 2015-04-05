64b4095d952cae7499e072146a4f499081122e76
hujun.zhao <hujun.zhao@chukong-inc.com>
2015-04-05 12:18:45 +0800

    change buy button

--- changeableball/Classes/BuyLifeLayer.cpp
+++ changeableball/Classes/BuyLifeLayer.cpp
@@ -60,23 +60,23 @@ void BuyLifeLayer::onEnter()
 
     CCSize size = CCDirector::sharedDirector()->getWinSize();
         
-    m_BuyItem = CCMenuItemImage::create("Images/startStandard.png","Images/startStandard.png", CC_CALLBACK_1(BuyLifeLayer::CallBuyLife,this));
-    m_BuyItem->setScale(CC_CONTENT_SCALE_FACTOR());
-    m_BuyItem->setTag(0);
+//    m_BuyItem = CCMenuItemImage::create("Images/startStandard.png","Images/startStandard.png", CC_CALLBACK_1(BuyLifeLayer::CallBuyLife,this));
+//    m_BuyItem->setScale(CC_CONTENT_SCALE_FACTOR());
+//    m_BuyItem->setTag(0);
+//    
+//    CCLabelTTF* testLabel1 = CCLabelTTF::create("复活 \n revive","ArialRoundedMTBold",16);
+//    testLabel1->setPosition(ccp(m_BuyItem->getContentSize().width/2,m_BuyItem->getContentSize().height/2));
+//    m_BuyItem->addChild(testLabel1);
+//    
     
-    CCLabelTTF* testLabel1 = CCLabelTTF::create("复活 \n revive","ArialRoundedMTBold",16);
-    testLabel1->setPosition(ccp(m_BuyItem->getContentSize().width/2,m_BuyItem->getContentSize().height/2));
-    m_BuyItem->addChild(testLabel1);
-    
-    
-    m_UseLife = CCMenuItemImage::create("Images/countdown.png","Images/countdown.png",CC_CALLBACK_1(BuyLifeLayer::CallUseLife,this));
+    m_UseLife = CCMenuItemImage::create("Images/fuhuo.png","Images/fuhuo.png",CC_CALLBACK_1(BuyLifeLayer::CallUseLife,this));
     m_UseLife->setScale(CC_CONTENT_SCALE_FACTOR());
     m_UseLife->setTag(rivive_button_tag);
-    CCLabelTTF* testLabel2 = CCLabelTTF::create("复活 \n revive","ArialRoundedMTBold",16);
-    testLabel2->setPosition(ccp(m_BuyItem->getContentSize().width/2,m_BuyItem->getContentSize().height/2));
-    m_UseLife->addChild(testLabel2);
+//    CCLabelTTF* testLabel2 = CCLabelTTF::create("复活 \n revive","ArialRoundedMTBold",16);
+//    testLabel2->setPosition(ccp(m_BuyItem->getContentSize().width/2,m_BuyItem->getContentSize().height/2));
+//    m_UseLife->addChild(testLabel2);
 
-    m_GameOver = CCMenuItemImage::create("Images/countdown.png","Images/countdown.png",CC_CALLBACK_1(BuyLifeLayer::gameOver,this));
+    m_GameOver = CCMenuItemImage::create("Images/gameover.png","Images/gameover.png",CC_CALLBACK_1(BuyLifeLayer::gameOver,this));
     m_GameOver->setScale(CC_CONTENT_SCALE_FACTOR());
     
 //    m_GameOver = CCMenuItemImage::create("Images/countdown.png","Images/countdown.png",CC_CALLBACK_1(BuyLifeLayer::gameOver,this));
@@ -103,33 +103,33 @@ void BuyLifeLayer::onEnter()
 //    addChild(m_labelLife,11);
     
     
-    m_iap6 = CCMenuItemImage::create("Images/startStandard.png","Images/startStandard.png", CC_CALLBACK_1(BuyLifeLayer::CallBuyLife,this));
+    m_iap6 = CCMenuItemImage::create("Images/zuan100.png","Images/zuan100.png", CC_CALLBACK_1(BuyLifeLayer::CallBuyLife,this));
     m_iap6->setScale(CC_CONTENT_SCALE_FACTOR());
     m_iap6->setTag(buy6_tag);
-    CCLabelTTF* testLabel = CCLabelTTF::create("复活 \n revive","ArialRoundedMTBold",16);
-    testLabel->setPosition(ccp(m_iap6->getContentSize().width/2,m_iap6->getContentSize().height/2));
-    m_iap6->addChild(testLabel);
-    m_iap18  = CCMenuItemImage::create("Images/startStandard.png","Images/startStandard.png", CC_CALLBACK_1(BuyLifeLayer::CallBuyLife,this));
+//    CCLabelTTF* testLabel = CCLabelTTF::create("复活 \n revive","ArialRoundedMTBold",16);
+//    testLabel->setPosition(ccp(m_iap6->getContentSize().width/2,m_iap6->getContentSize().height/2));
+//    m_iap6->addChild(testLabel);
+    m_iap18  = CCMenuItemImage::create("Images/zuan500.png","Images/zuan500.png", CC_CALLBACK_1(BuyLifeLayer::CallBuyLife,this));
     m_iap18->setScale(CC_CONTENT_SCALE_FACTOR());
     m_iap18->setTag(buy18_tag);
 
-    m_iap68 = CCMenuItemImage::create("Images/countdown.png","Images/countdown.png",CC_CALLBACK_1(BuyLifeLayer::CallBuyLife,this));
+    m_iap68 = CCMenuItemImage::create("Images/zuan2000.png","Images/zuan2000.png",CC_CALLBACK_1(BuyLifeLayer::CallBuyLife,this));
     m_iap68->setScale(CC_CONTENT_SCALE_FACTOR());
     m_iap68->setTag(buy68_tag);
     
-    auto item7= MenuItemFont::create("Quit", CC_CALLBACK_1(BuyLifeLayer::CallBuyLife, this));
+//    auto item7= MenuItemFont::create("Quit", CC_CALLBACK_1(BuyLifeLayer::CallBuyLife, this));
 
 //    CCMenu *menu = CCMenu::create(m_UseLife,m_iap6,m_iap18,m_iap68, m_GameOver, NULL);
     m_shopMenu = CCMenu::create(m_UseLife,m_iap6,m_iap18,m_iap68, m_GameOver, NULL);
-    m_shopMenu->alignItemsVerticallyWithPadding(10);
+    m_shopMenu->alignItemsVerticallyWithPadding(5);
     
     m_shopMenu->setPosition(ccp(size.width/2,size.height/2));
     this-> addChild(m_shopMenu);
     
-    Sprite* pDiamond = Sprite::create("Images/clock.png");
+    Sprite* pDiamond = Sprite::create("Images/icon_cash_l.png");
     pDiamond->setAnchorPoint(ccp(1,1));
     pDiamond->setScale(CC_CONTENT_SCALE_FACTOR());
-    pDiamond->setPosition(ccp(size.width*0.5 - 15,size.height-10));
+    pDiamond->setPosition(ccp(size.width*0.5 - 15,size.height-5));
     addChild(pDiamond);
     
     m_labelDiamond = CCLabelTTF::create("60","ArialRoundedMTBold",60);
