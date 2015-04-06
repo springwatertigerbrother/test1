8632d1e8abc5d1a0d37e896ef0a5a32a7db4bb64
hujun.zhao <hujun.zhao@chukong-inc.com>
2015-04-06 11:21:57 +0800

    add string encode function

--- changeableball/Classes/DataBase64.h
+++ changeableball/Classes/DataBase64.h
@@ -43,4 +43,25 @@ static void setIntegerForKey(const char* pKey, int value)
     //CCUserDefault::sharedUserDefault()->setStringForKey(pKey, data);//注释后，使用键值加密，开启，注释上句使用值加密
     CCUserDefault::sharedUserDefault()->flush();
 }
+
+static void setStringForKey(const char* pKey,const char* value)
+{
+    std::string data = value;
+    std::string key = pKey;
+    data = SaveData(reinterpret_cast<const unsigned char*>(data.c_str()), data.length());
+    key = SaveData(reinterpret_cast<const unsigned char*>(key.c_str()),key.length());
+    CCUserDefault::sharedUserDefault()->setStringForKey(key.c_str(), data);
+    //CCUserDefault::sharedUserDefault()->setStringForKey(pKey, data);//注释后，使用键值加密，开启，注释上句使用值加密
+    CCUserDefault::sharedUserDefault()->flush();
+}
+static std::string getStringForKey(const char* pKey)
+{
+    std::string key = pKey;
+    key = SaveData(reinterpret_cast<const unsigned char*>(key.c_str()), key.length());
+    std::string data = CCUserDefault::sharedUserDefault()->getStringForKey(key.c_str());
+    //std::string data = CCUserDefault::sharedUserDefault()->getStringForKey(pKey);//注释后，使用键值加密，开启，注释上句使用值加密
+    data = ParseData(data);
+    CCString *str = CCString::create(data);
+    return str->getCString();
+}
 #endif /* defined(__changeableball__DataBase64__) */
