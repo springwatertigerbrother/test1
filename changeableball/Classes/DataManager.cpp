6b468e7b80aae64a4b3b0eb9ae24f25b2afc957f
hujun.zhao <hujun.zhao@chukong-inc.com>
2015-04-08 11:34:24 +0800

    no message

--- changeableball/Classes/DataManager.cpp
+++ changeableball/Classes/DataManager.cpp
@@ -317,7 +317,11 @@ bool DataManager::init()
 
     CCLabelTTF* pLabelHelpCancel = CCLabelTTF::create();
     //    m_pTotalScoreLabel->setString(scoreStr);
+<<<<<<< HEAD
     pLabelHelpCancel->setPosition(ccp(s.width/2+100, s.height-50));
+=======
+    pLabelHelpCancel->setPosition(ccp(s.width/2+50, s.height-50));
+>>>>>>> 3e71852... local language
     pLabelHelpCancel->setVisible(true);
     pLabelHelpCancel->setColor(ccRED);
     //    m_pLabelHelp->setScale(2);
