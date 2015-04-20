3e74e321561134e39f638803efb45f24803824c0
hujun.zhao <hujun.zhao@chukong-inc.com>
2015-04-20 23:30:11 +0800

    bomb sound

--- changeableball/Classes/DataManager.cpp
+++ changeableball/Classes/DataManager.cpp
@@ -1146,10 +1146,10 @@ void DataManager::selectedTool(void* sender)
             break;
     }
 
-    if (!checkDiamondWithConsume(consumeDiamond, this))
-    {
-        return;
-    }
+//    if (!checkDiamondWithConsume(consumeDiamond, this))
+//    {
+//        return;
+//    }
     m_selectedTool = tag;
 
     pNode->runAction(Sequence::create(ScaleTo::create(0.3, 1.6*CC_CONTENT_SCALE_FACTOR()),ScaleTo::create(0.3, 1*CC_CONTENT_SCALE_FACTOR()),NULL));
@@ -1195,6 +1195,8 @@ void DataManager::useTool(CCPoint local)
         {
             usebomb(local);
             consumeDiamond(BOMB_COSUMED_DIAMOND);
+            CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("Sounds/bomb.mp3");
+            
         }
             break;
             
@@ -1202,6 +1204,8 @@ void DataManager::useTool(CCPoint local)
         {
             useWave(local);
             consumeDiamond(PULSE_COSUMED_DIAMOND);
+            CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("Sounds/pulse.mp3");
+
         }
             break;
             
