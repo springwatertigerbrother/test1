9dde00d15ae08f57c003291a6615579acde1bef6
hujun.zhao <hujun.zhao@chukong-inc.com>
2015-04-21 14:52:20 +0800

    fix no selected tool but tool will be used

--- changeableball/Classes/DataManager.cpp
+++ changeableball/Classes/DataManager.cpp
@@ -1182,6 +1182,11 @@ void DataManager::usebomb(CCPoint local)
 }
 void DataManager::useTool(CCPoint local)
 {
+    BallSprite * ds = getCurrentSelectSprite(local);
+    if (!ds)
+    {
+        return;
+    }
     switch (m_selectedTool)
     {
         case bomb:
